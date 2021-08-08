47cf1cf50e22eab01abdbed28752bd58d5c8fa8b
Simon Hartcher <simon@simonhartcher.com>
2021-08-08 21:29:59 +1000

    chore: Add tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "postcss-preset-env": {
       browsers: "last 2 versions"
     },
-    autoprefixer: {}
+    "postcss-css-variables": {},
+    tailwindcss: {},
+    autoprefixer: {},
   }
 };
