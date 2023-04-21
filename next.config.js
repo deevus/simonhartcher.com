e5a6248aa1b76e165650789665c2edac208eedce
Simon Hartcher <simon@simonhartcher.com>
2023-04-21 13:27:58 +1000

    fix(bio): add redirect to about page

--- next.config.js
+++ next.config.js
@@ -18,5 +18,14 @@ module.exports = withBundleAnalyzer({
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/bio',
+        destination: '/about',
+        permanent: false,
+      }
+    ]
+  },
 })
