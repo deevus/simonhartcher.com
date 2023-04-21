0b186d33fc6556c99e24b0951d06570c480bae8a
Simon Hartcher <simon@simonhartcher.com>
2023-04-21 16:08:45 +1000

    feat: add links to top bar

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,19 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '9f34e74cd7624be5a01e40cc37486161'
+    },
+    {
+      title: 'Contact',
+      pageId: '6292acebc23d4e798e90a6507a01f76b'
+    },
+    {
+      title: 'Newsletter',
+      pageId: '3771726a5f9e40e082803ff4c4592485',
+    }
+  ]
 })
