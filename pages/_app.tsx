2d09bffbc4b8a8aa0c08f55f2e101110b37b20db
Simon Hartcher <simon@simonhartcher.com>
2024-01-17 22:08:12 +1100

    chore: remove unused posthog variable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ import Analytics from 'analytics'
 
 // used for rendering equations (optional)
 import 'katex/dist/katex.min.css'
-import posthog from 'posthog-js'
 // used for code syntax highlighting (optional)
 import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
