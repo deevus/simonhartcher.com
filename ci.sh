78739dc7cec6d461aebe3c29d2fa9005d01d8311
Simon Hartcher <simon@simonhartcher.com>
2025-02-09 15:04:06 +1100

    ci: move to bash script

---
+++ ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+
+# Install Zig
+asdf plugin-add zig https://github.com/asdf-community/asdf-zig.git
+asdf set -u zig latest
+asdf install
+
+# Build project
+bun run build
