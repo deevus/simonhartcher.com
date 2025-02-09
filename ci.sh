569f9419fb5228df1b0eb5cd4a2b05aace44e3f4
Simon Hartcher <simon@simonhartcher.com>
2025-02-09 15:35:42 +1100

    ci: try installing zvm binary

--- ci.sh
+++ ci.sh
@@ -3,23 +3,8 @@
 # Fail on any error.
 set -e
 
-# If GOPATH is not set, set it to the default value
-if [ -z "$GOPATH" ]; then
-  export GOPATH="$HOME/go"
-  echo "GOPATH was not set. Updated to $GOPATH"
-fi
-
-# If GOBIN is not set, set it to the default value
-if [ -z "$GOBIN" ]; then
-  export GOBIN="$GOPATH/bin"
-  echo "GOBIN was not set. Updated to $GOBIN"
-fi
-
 echo "Installing zvm"
-go install -ldflags "-s -w" github.com/tristanisham/zvm@latest
-
-echo "Adding zvm bin to PATH"
-export PATH="$ZVM_PATH/bin:$PATH"
+curl https://raw.githubusercontent.com/tristanisham/zvm/master/install.sh | bash
 
 # Install Zig
 echo "Installing Zig"
