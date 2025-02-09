0ea8694c9117169424f1c4a332a15455befcfc89
Simon Hartcher <simon@simonhartcher.com>
2025-02-09 15:20:43 +1100

    ci: add gobin to path

--- ci.sh
+++ ci.sh
@@ -5,6 +5,11 @@ set -e
 
 # Install Zig
 export ZVM_PATH="$XDG_DATA_HOME/zvm"
+echo "ZVM_PATH=$ZVM_PATH"
+
+export PATH="$GOBIN:$PATH"
+echo "PATH=$PATH"
+
 go install -ldflags "-s -w" github.com/tristanisham/zvm@latest
 zvm install --force master
 zvm use master
