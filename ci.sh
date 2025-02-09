fdebd117a86dc37595c42b4ead239857fcde4bc9
Simon Hartcher <simon@simonhartcher.com>
2025-02-09 15:13:46 +1100

    ci: set tool version using echo

--- ci.sh
+++ ci.sh
@@ -3,9 +3,11 @@
 # Fail on any error.
 set -e
 
+# Set tool version for Zig
+echo "zig latest" > .tool-versions
+
 # Install Zig
 asdf plugin-add zig https://github.com/asdf-community/asdf-zig.git
-asdf set zig latest
 asdf install
 
 # Build project
